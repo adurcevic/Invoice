d2b4696a1e5c5b3375b444cfdec37eafa60eebb9
Antonio Durcevic <antonio.durcevic@gmail.com>
2024-07-15 15:32:37 +0200

    add build config

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,30 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { createSvgIconsPlugin } from 'vite-plugin-svg-icons'
+import { visualizer } from 'rollup-plugin-visualizer'
+import legacy from '@vitejs/plugin-legacy'
+import preset from 'postcss-preset-env'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+    createSvgIconsPlugin({
+      iconDirs: [fileURLToPath(new URL('./src/assets/icons', import.meta.url))],
+      symbolId: 'icon-[dir]-[name]'
+    }),
+    legacy(),
+    visualizer()
   ],
+  css: {
+    postcss: {
+      plugins: [preset()]
+    }
+  },
+  server: {
+    port: 3000
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
